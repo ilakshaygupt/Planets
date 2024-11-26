f4f8f4b417f5ae4cc461d336d60fe3f877b1b862
lakshay <gupta22154006@akgec.ac.in>
2024-11-26 10:45:04 +0530

    Initial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Lakshay Gupta on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
